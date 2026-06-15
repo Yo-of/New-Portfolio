cf7b2d3c985bce3a8a4b3f15927e7f166b1ceac7
Yo <mohammedlouali27@gmail.com>
2026-06-15 19:11:44 +0100

    Update

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 setInterval(function() {
     document.getElementById('timeElement').innerHTML = new Date().toLocaleString();
 }, 1000);
+
+dragElement(document.getElementById("welcome"));
+
+function dragElement(element) {
+    var initialX = 0;
+    var initialY = 0;
+    var currentX = 0;
+    var currentY = 0;
+
+    if (document.getElementById(element.id + "header")) {
+        document.getElementById(element.id +"header").onmousedown = startDragging;
+    } else {
+        element.onmousedown = startDragging;
+    }
+
+    function startDragging(e) {
+        e = e || window.event;
+        e.preventDefault();
+        initialX = e.clientX;
+        initialY = e.clientY;
+        document.onmouseup = stopDragging;
+        document.onmousemove = dragElement;
+    }
+
+}
